a8cf1ceca6c61f80ce22a088211643ee964f5fc3
Zurret <Zurret@users.noreply.github.com>
2023-04-13 17:55:41 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,10 +2,6 @@
  * 
  * MIT License
  * 
- * Copyrig/**
- * 
- * MIT License
- * 
  * Copyright (c) 2022 Zurret
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
